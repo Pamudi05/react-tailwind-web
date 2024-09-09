efa9d5fb92cd72bde88a577140105243273e47f7
Pamudi05 <113631350+Pamudi05@users.noreply.github.com>
2024-09-09 17:38:23 +0530

    responsive

--- src/component/footer.js
+++ src/component/footer.js
@@ -26,10 +26,10 @@ function Footer() {
   return (
     <>
       <div className="min-h-[150px] bg-black flex justify-start md:justify-center items-center md:items-center">
-        <footer className="m-4 text-center text-white">
-          <p className="text-sm">Frontend Practice - &copy; 2024 </p>
+        <footer className="m-4 ml-8 text-center text-white">
+          <p className="text-[15px]">Frontend Practice - &copy; 2024 </p>
           <div className="md:w-[800px] cursor-pointer p-4" ref={containerRef}>
-            <ul className="flex flex-col items-start flex-grow space-x-2 font-mono text-sm font-bold md:justify-center md:flex-row md:space-x-6">
+            <ul className="flex flex-col items-start flex-grow space-x-0 font-mono text-sm font-bold md:justify-center md:flex-row md:space-x-6">
               {items.map((item, index) => (
                 <li
                   key={index}
